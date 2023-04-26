b836b77b7eb87df2b5771399f5fe7e9f8af5d7c6
chrsm <git@c.chrsm.org>
2023-04-26 14:54:09 -0700

    cmd: exec script by filename

--- cmd/waffle/main.go
+++ cmd/waffle/main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"os"
 
 	"bits.chrsm.org/waffle"
+	"bits.chrsm.org/waffle/internal/luahttp"
+	"bits.chrsm.org/waffle/internal/luajson"
+	lua "github.com/yuin/gopher-lua"
 )
 
 func main() {
-	log.Println(waffle.Version, waffle.Date)
+	//log.Println(waffle.Version, waffle.Date)
+	_ = waffle.Version
+
+	vm := lua.NewState(lua.Options{
+		IncludeGoStackTrace: true,
+	})
+
+	luahttp.Preload(vm)
+	luajson.Preload(vm)
+
+	if err := vm.DoFile(os.Args[1]); err != nil {
+		fmt.Println(err)
+	}
 }
